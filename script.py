ac6896cf9a82eedbb9746df79f34f6cb77976dbf
stefanivanovic <stefanpooj@gmail.com>
2023-11-07 22:36:43 -0600

    Add files via upload

---
+++ script.py
@@ -0,0 +1,24 @@
+
+
+import sys
+import numpy as np
+
+from DeepCopy1_stefanivanovic99.pipeline import runEverything
+
+def getValuesSYS(listIn, keyList):
+    valueList = []
+    for key1 in keyList:
+        arg1 = np.argwhere(listIn == key1)[0, 0]
+        value1 = listIn[arg1+1]
+        valueList.append(value1)
+    return valueList
+
+
+if __name__ == "__main__":
+    keyList = ['-input', '-ref', '-output', '-refGenome']
+    listIn = np.array(sys.argv)
+    values1 = getValuesSYS(listIn, keyList)
+    bamLoc, refLoc, outLoc, refGenome = values1[0], values1[1], values1[2], values1[3]
+
+    runEverything(bamLoc, refLoc, outLoc, refGenome)
+
